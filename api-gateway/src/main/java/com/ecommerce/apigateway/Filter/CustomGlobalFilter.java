fbc2138c23e6bc08271f699eefb326d95b95bb98
KihaJeong1994 <jkha7371@gmail.com>
2022-02-22 18:21:15 +0900

    global filter 적용

---
+++ api-gateway/src/main/java/com/ecommerce/apigateway/Filter/CustomGlobalFilter.java
@@ -0,0 +1,29 @@
+package com.ecommerce.apigateway.Filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomGlobalFilter implements GlobalFilter,Ordered{
+
+    private  Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("custom global filter");
+        return chain.filter(exchange);
+    }
+    
+}
